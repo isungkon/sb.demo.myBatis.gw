c6a6531d86ed06065d0fbd09d1fa791f20b74f98
isungkon <isungkon@gmail.com>
2020-06-15 13:48:09 +0900

    comment @Test

--- demo.myBatis.GW/src/test/java/com/example/demo/ApplicationTests.java
+++ demo.myBatis.GW/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,29 +1,16 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.example.demo.repository.mariadb.city.City;
-import com.example.demo.service.CityService;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class ApplicationTests {
-	@Autowired
-	CityService service;
+	/*
+	 * @Autowired CityService service;
+	 * 
+	 * @Test void contextLoads() { }
+	 * 
+	 * @Test public void getCityById() { City city = service.getCityById(1);
+	 * System.out.println(city.getCountry()); }
+	 */
 	
-	@Test
-	void contextLoads() {
-	}
-	
-	@Test
-	public void getCityById() {
-		City city = service.getCityById(1);
-		System.out.println(city.getCountry());
-	}
-	
-
 }
