61c895635a468db33a8a6a4bc557598890b9f3d3
isungkon <isungkon@gmail.com>
2020-06-16 12:46:06 +0900

    springSecurity error - remove service call.

--- demo.myBatis.GW/src/main/java/com/example/demo/config/security/SpringSecurityConfig.java
+++ demo.myBatis.GW/src/main/java/com/example/demo/config/security/SpringSecurityConfig.java
@@ -1,18 +1,14 @@
 package com.example.demo.config.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import com.example.demo.repository.mariadb.city.City;
-import com.example.demo.service.CityService;
-
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	CityService service;
+	//@Autowired
+	//CityService service;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -20,9 +16,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		//call service for reading auth policy from resources.
 		int id = 1;
-		City city = service.getCityById(id);
+		//City city = service.getCityById(id);
 		
-		System.out.println(city.getCountry());
+		//System.out.println(city.getCountry());
 		
 		http.authorizeRequests()
 				.antMatchers("/").permitAll()
