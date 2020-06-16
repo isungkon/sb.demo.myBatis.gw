2b477e1e18bc83fb2be23e6cdd17d4ace5fa9c12
isungkon <isungkon@gmail.com>
2020-06-16 15:08:12 +0900

    springsecurity - using daon in configuration class.

--- demo.myBatis.GW/src/main/java/com/example/demo/config/security/SpringSecurityConfig.java
+++ demo.myBatis.GW/src/main/java/com/example/demo/config/security/SpringSecurityConfig.java
@@ -1,29 +1,25 @@
 package com.example.demo.config.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import com.example.demo.repository.mariadb.city.City;
-import com.example.demo.service.CityService;
-
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	@Autowired
-	CityService service;
+//	@Autowired
+//	CityService service;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		//call service for reading auth policy from resources.
-		int id = 1;
-		City city = service.getCityById(id);
+//		int id = 1;
+//		City city = service.getCityById(id);
 		//System.out.println();
 		
 		
-		System.out.println("[SpringSecurityConfig][configure]" + city.getCountry());
+		System.out.println("[SpringSecurityConfig][configure]");
 		
 		http.authorizeRequests()
 				.antMatchers("/").permitAll()
